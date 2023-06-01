9263bd17a4a1244d2a0756661460b1875c343ca1
HakanKara75 <hakanbatirhan@gmail.com>
2023-06-01 23:12:12 +0300

    v

--- src/test/java/techproed/runners/Runner.java
+++ src/test/java/techproed/runners/Runner.java
@@ -27,7 +27,7 @@ senaryoyu çalıştırabiliriz
         glue = {"techproed/stepdefinitions"},
         //Bu parametre ile kodlarımızı yazdığımız stepDefinition
         //class'ının packege'ını belirtiriz
-        tags = "@productfilter",
+        tags = "@hakan",
         dryRun = false,
         //dryRun=false Test adımlarını kontrol eder ve browser'ı çalıştırır
         //dryRun=True Test adımlarını sadece kontrol eder
